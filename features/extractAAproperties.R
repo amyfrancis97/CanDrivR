161cfb54fde023928edfa55d7e2e1a603a812c8a
amyfrancis97 <amyfrancis1997@hotmail.com>
2023-01-26 09:48:07 +0000

    Updating amino acid script to new features (extended feature set)

--- features/extractAAproperties.R
+++ features/extractAAproperties.R
@@ -16,52 +16,7 @@ AA=read.table("/Users/uw20204/Desktop/PhD/AAforRPeptidesPackage.txt", sep = ",",
 # Drop any variants where amino acids are unkown in the dataset
 AA = AA[AA['AA1'] != "-", ]
 AA = AA[AA['AA2'] != "-", ]
-################### didnt use this in the end as everything is included in the second table
-getAminoAcidProperties = function(AA1, AA2){
-  aaPropertyChange = list()
-  for(aa in c(AA1, AA2)){
-    propertyDfFull2 = c()
-    for(property in AAdata){
-      propertyDfFull = data.frame()
-      for(subproperty in 1:length(property)){
-        feature = names(property)[subproperty]
-        if(aa %in% names(property[[subproperty]])){
-          values = property[[subproperty]][names(property[[subproperty]]) == aa][[1]]
-        }else{
-          values = NA
-        }
-        df = data.frame(feature, values)
-        propertyDfFull = rbind(propertyDfFull, df)
-      }
-      propertyDfFull2 = rbind(propertyDfFull2, propertyDfFull)
-    }
-    aaPropertyChange[[aa]] = propertyDfFull2
-  }
-  return(aaPropertyChange)
-}
 
-AAdf1 = list()
-for(row in 1:nrow(AA)){
-  AA1 = AA[row, 'AA1']
-  AA2 = AA[row, 'AA2']
-  aaPropertyChange = getAminoAcidProperties(AA1, AA2)
-  if(length(aaPropertyChange) == 2){
-    AAdf = merge(aaPropertyChange[[1]], aaPropertyChange[[2]], by = "feature", all = TRUE)
-  }else{
-    AAdf = merge(aaPropertyChange[[1]], aaPropertyChange[[1]], by = "feature", all = TRUE)
-  }
-  AAdf1[[row]] = AAdf
-}
-
-AAdf2 = data.frame(matrix(ncol = length(c(paste("AA1",AAdf1[[1]][, 'feature'],sep="_"), paste("AA2",AAdf1[[1]][, 'feature'],sep="_")))))
-colnames(AAdf2) = c(paste("AA1",AAdf1[[1]][, 'feature'],sep="_"), paste("AA2",AAdf1[[1]][, 'feature'],sep="_"))
-
-for(i in 1:length(AAdf1)){
-  AAdf2[i, ] = c(AAdf1[[i]][, 2], AAdf1[[i]][, 3])
-}
-
-AAdf2 = cbind(AA[, 1:4], AAdf2)
-####################################################
 ########## GET OTHER PROPERTIES ########
 load("/Users/uw20204/Downloads/AAindex.rda")
 
@@ -84,10 +39,8 @@ getAAExtraProperties = function(variant){
 x = 1:nrow(AAdf2)
 lists=lapply(x, getAAExtraProperties)
 df = do.call(rbind, lists)
-colnames(AAdf2)
 colnames(df) = c(paste("AA1", AAindex$name, sep = "_"), paste("AA2", AAindex$name, sep = "_"))
 AAdf2 = cbind(AA[, 1:4], df)
-AAdf2[1, ]
 write.table(AAdf2, "/Users/uw20204/Desktop/PhD/VEP_web_aminoAcid_properties2.txt", quote = FALSE, row.names = FALSE, sep = "\t")
 
 
