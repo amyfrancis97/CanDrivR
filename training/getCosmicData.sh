992f755050ce0e1b1d1bae72924ab8563cf40c03
amyfrancis97 <amyfrancis1997@hotmail.com>
2023-03-09 10:57:35 +0000

    automating cosmic script fo both coding and non-coding cosmic datasets

--- training/getCosmicData.sh
+++ training/getCosmicData.sh
@@ -3,26 +3,41 @@
 #SBATCH --partition=cnu
 #SBATCH --time=14-00:00:0
 #SBATCH --mem=100G
-#SBATCH --chdir=/bp1/mrcieu1/data/encode/public/cosmicGnomad_20230215
+#SBATCH --chdir=/bp1/mrcieu1/data/encode/public/CanDrivR/training/non-coding/
 #SBATCH --account=sscm013903
 
 module load apps/bedops apps/bedtools
 
-###############################################################	Cosmic Data #############
+############### Get Cosmic Coding Data ############
 # Get the cosmic dataset
 #wget https://cancer.sanger.ac.uk/cosmic/file_download/GRCh38/cosmic/v97/CosmicMutantExport.tsv.gz
 
 # unzip
 #gunzip CosmicMutantExport.tsv.gz
 
+############### Get Cosmic Non-Coding Data ##########
+#wget https://cancer.sanger.ac.uk/cosmic/file_download/GRCh38/cosmic/v97/CosmicNCV.tsv.gz
+
+# unzip
+#gunzip CosmicNCV.tsv.gz
+
+filter_cosmic() {
+
+echo $1
+
 # Convert from TSV to CSV
-#tr '\t' ',' < CosmicMutantExport.tsv > CosmicMutantExport.csv 
+tr '\t' ',' < $1.tsv > $1.csv 
 
 # Remove white spaces in column names
 # Select only SNVs (those with arrow symbol)
-#sed '1s/ /_/g'  CosmicMutantExport.csv | awk -F, '$37 ~ '/'>'/'' > CosmicMutantExport.tmp 
-#mv CosmicMutantExportNoSpaces.csv CosmicMutantExport.csv
+sed '1s/ /_/g'  $1.csv | awk -F, -v var=$2 '$var ~ '/'>'/'' > $1.tmp 
+mv $1.tmp $1.csv
 
 # Gets recurrence of each SNV (how many different sample ID contain the variant)
-python /bp1/mrcieu1/data/encode/public/cosmicGnomad_20230215/finalV2Scripts/getReccurence.py
+python /bp1/mrcieu1/users/uw20204/paper1/training/getReccurence.py $1 $2
 
+}
+# Execute function for cosmic coding and non-coding
+# Number represents HGVSG column in dataframe
+filter_cosmic CosmicNCV 24
+#filter_cosmic CosmicMutantExport 37
--- training/getReccurence.py
+++ training/getReccurence.py
@@ -5,26 +5,27 @@ import re
 from textwrap import wrap
 import numpy as np
 from config import *
+import sys 
 
-dfList = []
-# reads in dataframe in chunks
-with pd.read_csv(cosmicMutantExportCSV, on_bad_lines='skip', chunksize=100000, sep = ",", encoding= 'unicode_escape', header = None) as reader:
-    for chunk in reader:
-        chunk = chunk.reset_index(drop=True)
-        chunk = chunk[chunk[36].str.contains(">", na=False)] # Filter for substitutions only
-        chunk.insert(0, 'chrom', chunk[36].str.split(":", expand = True)[0])
-        chunk.insert(1, 'start', chunk[36].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[:-1])
-        chunk.insert(2, 'end', chunk[36].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[:-1])
-        chunk.insert(3, 'ref_allele', chunk[36].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[-1:])
-        chunk.insert(4, 'alt_allele',chunk[36].str.split(">", expand = True)[1])
-        dfList.append(chunk)
-dfList = pd.concat(dfList)
-print(dfList.head())
-# drop variants whos location is not properly defined
-dfList = dfList.drop((dfList[dfList['chrom'].isna()].index), axis = 0)
-#dfList['R'] = dfList.groupby([36])[36].transform('count')
-#dfList['R'] = dfList.groupby([36])[5].transform('nunique').tolist()
-#dfList = dfList.drop_duplicates(subset=[35], keep = 'first')
-dfList = dfList.groupby(['chrom', 'start', 'end', 'ref_allele', 'alt_allele'], as_index=False).size() # Gets the reccurence of each variant
-print(dfList.head())
-dfList.to_csv("CosmicMutantExportNew.bed", sep = "\t", index = None) # Exports as a bed file
+if __name__ == "__main__":
+    dfList = []
+    col = int(sys.argv[2])-1
+    file = sys.argv[1]
+    # reads in dataframe in chunks
+    with pd.read_csv(file + ".csv", on_bad_lines='skip', chunksize=100000, sep = ",", encoding= 'unicode_escape', header = None) as reader:
+        for chunk in reader:
+            chunk = chunk.reset_index(drop=True)
+            chunk = chunk[chunk[col].str.contains(">", na=False)] # Filter for substitutions only
+            chunk.insert(0, 'chrom', chunk[col].str.split(":", expand = True)[0])
+            chunk.insert(1, 'start', chunk[col].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[:-1])
+            chunk.insert(2, 'end', chunk[col].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[:-1])
+            chunk.insert(3, 'ref_allele', chunk[col].str.split(":", expand = True)[1].str.split("g.", expand = True)[1].str.split(">", expand = True)[0].str[-1:])
+            chunk.insert(4, 'alt_allele',chunk[col].str.split(">", expand = True)[1])
+            dfList.append(chunk)
+    dfList = pd.concat(dfList)
+    print(dfList.head())
+    # drop variants whos location is not properly defined
+    dfList = dfList.drop((dfList[dfList['chrom'].isna()].index), axis = 0)
+    dfList = dfList.groupby(['chrom', 'start', 'end', 'ref_allele', 'alt_allele'], as_index=False).size() # Gets the reccurence of each variant
+    print(dfList.head())
+    dfList.to_csv(sys.argv[1] + ".bed", sep = "\t", index = None) # Exports as a bed file
